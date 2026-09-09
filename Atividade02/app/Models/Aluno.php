c5878550f673cb1a3425686d3d93a6afaa60adc7
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-09-09 12:13:28 -0300

    User com Aluno

--- Atividade02/app/Models/Aluno.php
+++ Atividade02/app/Models/Aluno.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,4 +14,9 @@ class Aluno extends Model
         'email',
         'curso',
     ];
+
+    public function user()
+{
+    return $this->belongsTo(User::class);
+}
 }
