e9f287e875e973abd5727d7516dcf0a9b9a65ffc
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-09-08 21:09:26 -0300

    Busca todos os alunos do banco de dados  Retorna para view com lista de alunos

--- Atividade02/app/Http/Controllers/AlunoController.php
+++ Atividade02/app/Http/Controllers/AlunoController.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class AlunoController
 {
-    //
+     public function index()
+    {
+        $alunos = Aluno::all();
+        
+        return view('alunos.index', compact('alunos'));
+    }
 }
