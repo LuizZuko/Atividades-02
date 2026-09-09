e1c19befa56de3ced52bc5a2bdf68aa8be14655c
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-09-09 01:02:40 -0300

    pasta aluno

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'myapp/atividades-02',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1d7dc4f5ac12e3201d3a3a03416b941910214342',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'myapp/atividades-02' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1d7dc4f5ac12e3201d3a3a03416b941910214342',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
