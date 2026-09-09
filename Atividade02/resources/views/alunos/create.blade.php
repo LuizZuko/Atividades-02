4f700a5f758882c86cbcbeb2be943bb25fc59ac0
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-09-09 01:12:50 -0300

    limpa e padroniza o formulário de cadastro de alunos

--- Atividade02/resources/views/alunos/create.blade.php
+++ Atividade02/resources/views/alunos/create.blade.php
@@ -1,3 +1,24 @@
 <div>
     <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
 </div>
+@extends('layouts.app')
+
+@section('title', 'Cadastrar Aluno')
+
+@section('content')
+
+<h1>Cadastrar Aluno</h1>
+
+<form action="{{ route('alunos.store') }}" method="POST">
+    @csrf
+
+    <label>Nome:</label>
+    <input type="text" name="nome" required>
+
+    <label>Email:</label>
+    <input type="email" name="email" required>
+
+    <button type="submit">Cadastrar</button>
+</form>
+
+@endsection
