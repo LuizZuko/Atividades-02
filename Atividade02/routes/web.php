fe2ce98c7d7a644d1b7f00eaf98c633905083bf8
Alex37087 <engs-alexbecker@camporeal.edu.br>
2026-09-09 12:10:29 -0300

    adiciona rotas de autenticação (login, autenticar e logout

--- Atividade02/routes/web.php
+++ Atividade02/routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'login'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'autenticar'])
+    ->name('login.autenticar');
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
 Route::get('/', function () {
     return view('welcome');
 });
